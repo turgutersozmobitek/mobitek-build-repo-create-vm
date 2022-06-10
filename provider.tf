e2e847d81ad7268cd0a0da9eea83946177f4578f
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-06-10 12:52:18 +0300

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    vcd = {
+      source = "vmware/vcd"
+      version = "3.6.0"
+    }
+  }
+}
+
+# Connect VMware vCloud Director Provider
+provider "vcd" {
+  user                 = var.vcd_user
+  password             = var.vcd_pass
+  org                  = "System"
+  vdc                  = var.vdc_name
+  url                  = var.vcd_url
+  max_retry_timeout    = var.vcd_max_retry_timeout
+  allow_unverified_ssl = var.vcd_allow_unverified_ssl
