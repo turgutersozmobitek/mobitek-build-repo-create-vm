5ebcaa777048b149f9f4586fb080590597fc3ac3
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-06-16 11:49:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
 #bir oluşturalım 
-
 resource "vcd_vapp_org_network" "vappOrgNet" {
   org = var.org_name
   vdc = var.vdc_name
   vapp_name = var.vapp_name
-
   # Comment below line to create an isolated vApp network
   org_network_name = var.org_network_name
 }
-
 resource "vcd_vapp_vm" "web1" {
   vapp_name     = var.vapp_name
   name          = var.vm_name
@@ -20,23 +17,18 @@ resource "vcd_vapp_vm" "web1" {
   cpu_hot_add_enabled=true
   memory_hot_add_enabled=true
   power_on=true
-
     override_template_disk {
     bus_type        = "paravirtual"
-    size_in_mb      = "102400"
+    size_in_mb      = var.vm_disk_size
     bus_number      = 0
     unit_number     = 0
     iops            = 0
     storage_profile = "*"
   }
-
-
-
   guest_properties = {
     "guest.hostname"   = "my-host"
     "another.var.name" = "var-value"
   }
-
   network {
     type               = "org"
     name               = var.org_network_name
@@ -45,12 +37,7 @@ resource "vcd_vapp_vm" "web1" {
     adapter_type       = "VMXNET3"
     is_primary         = true
   }
-
    customization{
       enabled=false
     }
-
-
 }
-
-
