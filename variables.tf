b20d8ad696825372e1974c1b8f7d063657f350b2
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-07-08 11:30:41 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,59 +11,61 @@ variable "vdc_name" {
 
 variable "vapp_name" {
     type        = string
-    description = "VAPP Adı:*"
+    description = "VAPP Adı:"
 }
 
 variable "org_network_name" {
     type        = string
-    description = "vApp için kullanılacak Organizasyon Network adını yazınız."
+    description = "Netwwrk Adı:"
 }
 
 variable "vm_name" {
     type        = string
-    description = "Oluşturulacak vM adını giriniz."
+    description = "Sunucu Adı:"
 }
 
 variable "vm_description" {
     type        = string
-    description = "vM Açıklaması - UNMANAGED - MANAGED"
+    description = "Sunucu AçıkLaması:"
 }    
 
 variable "catalog_name" {
     type        = string
-    description = "Kullanılacak imajın olduğu Catalog ismini giriniz."
+    description = "Catalog:"
 }
 
 variable "template_name" {
   type        = string
-  description = "Catalog içinde kullanılacak Template adını giriniz Örn: Windows2019 gibi"
+  description = "OS:"
   default     = "Windows2019"
 }
 
 
-
-variable "vm_memory" {
-    type        = string
-    description = "Sunucuya verilecek Memory miktarını MB cinsinden yazınız. 1GB = 1024"
-}
-
 variable "vm_cpu" {
     type        = string
-    description = "Sunucuya verilecek CPU adedini giriniz."
+    description = "CPU:"
 }
 
 variable "vm_cpu_core" {
     type        = string
-    description = "Sunucuya verilecek CPU Core adedini giriniz."
+    description = "CPU Core:"
+}
+
+variable "vm_disk_size" {
+    type        = string
+    description = "Disk:"
+}
+
+
+variable "vm_memory" {
+    type        = string
+    description = "Memory:"
 }
 
 
 variable "vm_ip" {
     type        = string
-    description = "Sunucuya verilecek IP adresini giriniz."
+    description = "IP Adresi:"
 }
 
-variable "vm_disk_size" {
-    type        = string
-    description = "Disk Boyutunu MB cinsinden giriniz. 1GB = 1024"
-}
+
