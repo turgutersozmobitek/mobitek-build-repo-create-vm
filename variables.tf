d042b80f3c5d3b301185af02cbf7676c09af38ed
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-08-09 23:35:58 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,64 +16,74 @@ variable "cd_vdcadres" {
 variable "org_name" {
     type        = string
     description = "Organizasyon İsmi:"
+    default     = "list"
 }
 
 variable "vdc_name" {
     type        = string
     description = "VDC Adı:"
+    default     = "list"
+
 }
 
 
 variable "vapp_name" {
     type        = string
     description = "VAPP Adı:"
+    default     = "list"
 }
 
 
 variable "vm_cpu" {
     type        = string
     description = "CPU:"
+    default     = "list"
 }
 
 variable "vm_cpu_core" {
     type        = string
     description = "CPU Core:"
+    default     = "list"
 }
 
 
 variable "vm_memory" {
     type        = string
     description = "Memory:"
+    default     = "list"
 }
 
 variable "vm_disk_size" {
     type        = string
     description = "Disk:"
+    default     = "list"
 }
 
 
 variable "org_network_name" {
     type        = string
     description = "Network Adı:"
+    default     = "list"
 }
 
 
 variable "vm_description" {
     type        = string
     description = "MANAGED/UNMANAGED:"
+    default     = "list"
 }    
 
 variable "catalog_name" {
     type        = string
     description = "Catalog:"
+    default     = "list"
 }
 
 variable "template_name" {
   type        = string
   description = "OS:"
-  default     = "Windows2019"
+  default     = "list"
 }
-
 
 
 variable "vm_ip" {
